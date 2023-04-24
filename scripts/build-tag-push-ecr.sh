d0a2aebf18fd1c96b512a9d9ef452455abad9791
abdul-gds <104900842+abdul-gds@users.noreply.github.com>
2023-04-24 10:40:10 +0100

    Update build-tag-push-ecr.sh

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -3,8 +3,10 @@
 set -eu
 
 echo "building image(s)"
-pwd 
-ls -la 
+
+# cd "${WORKING_DIRECTORY}"
+# echo "Packaging app in /$WORKING_DIRECTORY"
+
 docker build -t "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA" -f $DOCKERFILE .
 docker push "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"
 cosign sign --key "awskms:///${CONTAINER_SIGN_KMS_KEY_ARN}" "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"
